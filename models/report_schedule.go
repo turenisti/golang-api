5d9b2c5e70f6428fbb4ec0d3c5edb6f11dcccbd2
Arif Wahyu Setiawan <arif@finnet-indonesia.com>
2025-11-13 12:05:13 +0700

    fix inactive creation

--- models/report_schedule.go
+++ models/report_schedule.go
@@ -6,7 +6,7 @@ type ReportSchedule struct {
 	ConfigID       int         `gorm:"not null;index;column:config_id" json:"config_id"`
 	CronExpression string      `gorm:"size:100;not null;column:cron_expression" json:"cron_expression"`
 	Timezone       string      `gorm:"size:50;default:'UTC';column:timezone" json:"timezone"`
-	IsActive       bool        `gorm:"not null;default:1;index;column:is_active" json:"is_active"`
+	IsActive       bool        `gorm:"not null;index;column:is_active" json:"is_active"`
 	LastRunAt      *CustomTime `gorm:"column:last_run_at" json:"last_run_at"`
 	NextRunAt      *CustomTime `gorm:"column:next_run_at" json:"next_run_at"`
 	CreatedAt      CustomTime  `gorm:"default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
